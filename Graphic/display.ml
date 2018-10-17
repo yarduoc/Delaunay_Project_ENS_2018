475f64f4d70227579eacbcf2dd84c7385fcd3e36
yarduoc <alexmadrid@free.fr>
2018-10-17 10:49:43 +0200

    Début de la branche graphique

--- Graphic/display.ml
+++ Graphic/display.ml
@@ -1,3 +1,7 @@
+#load "graphics.cma"
+open Graphics
+#use "./common/main.ml"
+
 let draw_point (x:point_set) = ();;
 let draw_triangle (x:triangle_set) = ();;
 let delaunay_stepwise (x:point_set) = ();;
