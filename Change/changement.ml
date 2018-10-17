d8f7f6fff39a70f22d273c18eeee7b047f56d21b
yarduoc <alexmadrid@free.fr>
2018-10-17 17:52:41 +0200

    maj branche paul

--- Change/changement.ml
+++ Change/changement.ml
@@ -1,29 +1,30 @@
 #use "common/main.ml"
 #use "common/alphaset.ml"
 
-let get_line tri_set =
+let get_line (tri_set:triangle_set) =
     let result_line_set = ref (empty()) in
-    let add_line cur_tri =
-        result_line_set := (cur_tri.p1,cur_tri.p2) :: (cur_tri.p2,cur_tri.p3) :: (cur_tri.p3,cur_tri.p1) :: (!result_line_set)
-    in iter add_line tri_set; !result_line_set;;
+    let add_line curr_tri =
+        result_line_set := (curr_tri.p1,curr_tri.p2) :: (curr_tri.p2,curr_tri.p3) :: (curr_tri.p3,curr_tri.p1) :: (!result_line_set)
+    in iter add_line tri_set; !result_line_set
+;;
 
-
-let border_aux line_set =
-    let ots_set_line = ref (empty()) in
-    let sup_set_line = ref (empty()) in
-    let result_set_line = ref line_set in
-    let find_aux cur_line =
-        if  not (find !sup_set_line cur_line) then
-            if find cur_line (!ots_set_line) then
-                sup_set_line := cur_line::(!sup_set_line);
-        if not (find !ots_set_line cur_line) then
-            ots_set_line := cur_line::(!sup_set_line);
+let border_aux (line_set:(point*point) set) =
+    let seen_line_set = ref (empty()) in
+    let suppr_line_set = ref (empty()) in
+    let result_line_set = ref line_set in
+    let find_aux curr_line =
+        if  not (find !suppr_line_set curr_line) then
+            if find !seen_line_set curr_line then
+                suppr_line_set := curr_line::(!suppr_line_set);
+        if not (find !seen_line_set curr_line) then
+            seen_line_set := curr_line::(!seen_line_set);
     in iter find_aux line_set;
-    let sup_aux cur_line =
-        if find !sup_set_line cur_line then
-            suppress result_set_line cur_line
-    in iter sup_aux result_set_line;
-    result_set_line;;
+    let suppr_aux curr_line =
+        if find !suppr_line_set curr_line then
+            suppress result_line_set curr_line
+    in iter suppr_aux !result_line_set;
+    !result_line_set
+;;
 
 let border tri_set = border_aux (get_line tri_set);;
 
