e19d1274b44874c0e20f25370ad9f425469a3275
Desauw <lauric.desauw@ens-rennes.fr>
2018-10-17 10:49:57 +0200

    update intefaces

--- Detection/detec.mli
+++
@@ -1,4 +0,0 @@
-type point = { x : float; y : float; }
-type triangle = { p0 : point; p1 : point; p3 : point; }
-val is_counterclockwise : point -> point -> point -> bool
-val in_circle : triangle -> point -> bool
