861b871dd62ee8d345e8dec25b4002dad97ba975
Desauw <lauric.desauw@ens-rennes.fr>
2018-10-17 16:48:26 +0200

    matrice compile

--- Detection/detec.ml
+++ Detection/detec.ml
@@ -7,16 +7,28 @@ let is_counterclockwise (p1:point) (p2:point) (p3:point) =  (*Is correct*)
 ;;
 
 let in_circle (t:triangle) (curr_point:point) =
-  let in_matrix = change( 1 1 (t.p1.x -. curr_point.x)
-                  (change( 1 2 (t.p1.y -. curr_point.y)
-                  (change 1 3  ( (t.p1.x -. curr_point.x)*.(t.p1.x -. curr_point.x) +. (t.p1.y -. curr_point.y)*.(t.p1.y -. curr_point.y))
-                  (change( 1 1 (t.p2.x -. curr_point.x)
-                  (change( 1 2 (t.p2.y -. curr_point.y)
-                  (change(1 3 ( (t.p2.x -. curr_point.x)*.(t.p2.x -. curr_point.x) +. (t.p2.y -. curr_point.y)*.(t.p2.y -. curr_point.y))
-                  (change( 1 1 (t.p3.x -. curr_point.x)
-                  (change( 1 2 (t.p3.y -. curr_point.y)
-                  (change 1 3 ( (t.p3.x -. curr_point.x)*.(t.p3.x -. curr_point.x) +. (t.p3.y -. curr_point.y)*.(t.p3.y -. curr_point.y))
-                  (cr_m33))))))))))))))) in
+  let in_matrix =  change 1 1 (t.p1.x -. curr_point.x)
+                  (change 1 2 (t.p1.y -. curr_point.y)
+                  (change 1 3  (   (t.p1.x -. curr_point.x)
+                                 *.(t.p1.x -. curr_point.x)
+                                 +.(t.p1.y -. curr_point.y)
+                                 *.(t.p1.y -. curr_point.y)
+                               )
+                  (change 2 1 (t.p2.x -. curr_point.x)
+                  (change 2 2 (t.p2.y -. curr_point.y)
+                  (change 2 3  (   (t.p1.x -. curr_point.x)
+                                 *.(t.p1.x -. curr_point.x)
+                                 +.(t.p1.y -. curr_point.y)
+                                 *.(t.p1.y -. curr_point.y)
+                               )
+                  (change 3 1 (t.p3.x -. curr_point.x)
+                  (change 3 2 (t.p3.y -. curr_point.y)
+                  (change 3 3  (   (t.p1.x -. curr_point.x)
+                                 *.(t.p1.x -. curr_point.x)
+                                 +.(t.p1.y -. curr_point.y)
+                                 *.(t.p1.y -. curr_point.y)
+                               )
+                  (cr_m33() ))))))))) in
 
   let det = det_3 in_matrix in
   if is_counterclockwise (t.p1) (t.p2) (t.p3)
--- Detection/matrix.ml
+++ Detection/matrix.ml
@@ -1,67 +1,43 @@
+(*Builders and modifiers*)
+let cr_m33 () = Array.make_matrix 3 3 0. ;;
+
 (*Getters*)
 
-let index l i = (*give the i-eme element of l*)
-  let rec aux l i k = match l with
-    |[] -> failwith "index"
-    |t::q -> if i = k then t else aux l i (k+1);
+let indice m i j = m.(1).(j);; (* give M[i,j]*)
 
-    in aux l i 0;;
-
-let indice m i j = index (index m i) j;; (* give M[i,j]*)
-
-let rec shape m = match m with
-  |t::q ->  1 + shape q
-  |_ -> 0;;
-
-(*Builders and modifiers*)
-let cr_m33 () =
-  let  matrice = ref [] in
-  for k=0 to 3
-
-    do let line= ref [] in
-    for l=0 to 3
-      do begin  line := 0::(!line)
-        end;
-      done;
-      matrice := (!line)::(!matrice)
-    done;
-    (!matrice);;
-
-let replace_list l i x =
-  let rec aux l i x k =
-    match l with
-      |[] -> failwith "replace"
-      |t::q -> if k = i then x::q
-                else t::(aux l i x (k+1))
-  in aux l i x 0;;
-
-let change i j x matrice  =
-  replace_list matrice i (replace_list (index matrice i) j x);;
+let rec shape m = Array.length  m
 
 (*Calculus methods*)
-let mineur m  i j = (*give the minor of m develop with M[i,j]*)
-  let  mineur_m = ref [] in
-  for k=0 to shape m
-
-    do let mini = ref [] in
-    for l=0 to shape m
-
-      do if (k !=i && l != j)
-       then begin  mini := (indice m k l)::(!mini)
-        end;
-      done;
-      mineur_m := (!mini)::(!mineur_m)
-    done;
-    det_2 (!mineur_m);;
-
 
 let det_2 m = (*determinant 2*2 *)
-    (indice m 1 1) *.(indice m 2 2) -. (indice m 1 2) *.(indice m 2 1)
+    (indice m 0 0) *.(indice m 1 1) -. (indice m 0 1) *.(indice m 1 0)
     ;;
 
+let mineur m i j = (*give the minor of m develop with M[i,j]*)
+  let  minor = Array.make_matrix 2 2 0. in
+  let a = ref 0 and b = ref 0 in
+  for k=0 to 2 do
+    for l=0 to 2 do
+      if (k !=i && l != j && !a < 2 )
+       then begin minor.(!b).(!a) <-  m.(k).(l);
+          a:= 1 + (!a); print_int (!a)
+            end;
+      done;
+      if (k !=i && !b < 2)
+        then  begin
+              b := 1+ (!b);
+              a:= 0
+              end;
+
+    done;
+    print_string "det";
+    det_2 minor;;
+
+
+
 let det_3 m = (*determinant 3*3 *)
-  let c_12 = -.(mineur m 1 2)in
-  let c_22 = (mineur m 2 2)in
-  let c_32 = -. (mineur m 3 2)in
+  let c_12 = -.(mineur m 0 1)in
+  let c_22 = (mineur m 1 1)in
+  let c_32 = -. (mineur m 2 1)in
   (indice m 1 2)*.c_12 +. (indice m 2 2)*.c_22 +. (indice m 3 2)*.c_32
   ;;
