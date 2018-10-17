35758f9a32eb937c5dae88b98364add89e9cb8d2
Desauw <lauric.desauw@ens-rennes.fr>
2018-10-17 14:24:31 +0200

    matrix modifier

--- Detection/detec.ml
+++ Detection/detec.ml
@@ -7,18 +7,6 @@ let is_counterclockwise (p1:point) (p2:point) (p3:point) =  (*Is correct*)
 ;;
 
 let in_circle (t:triangle) (curr_point:point) =
-  let a_11 = in
-  let a_12 = in
-  let a_13 = in
-  let a_21 = in
-  let a_22 = in
-  let a_23 = in
-  let a_31 = in
-  let a_32 = in
-  let a_33 = in
-  let c_12 = in
-  let c_22 = in
-  let c_32 = in
-  let det =   a_11*.c_12 +. a_22*.c_22 +. a_32*.c_32 in
+
 
   det > 0. ;;
