27cec1ae01dda460102d03031272e364aa77317d
PaulBastide <paul.bastide@ens-rennes.fr>
2018-10-17 15:15:39 +0200

    save 17/10 15:15

--- Change/changement.ml
+++ Change/changement.ml
@@ -1,13 +1,33 @@
-let rec find l elm = match l with
-|  
+
+#load "set.cma";;
+open Set;;
+
+let get_line tri_set =
+    let result_line_set = ref empty() in
+    let add_line cur_tri =
+        result_line_set := cur_tri.p1 :: cur_tri.p2 :: cur_tri.p3 :: (!result_line_set)
+    in iter add_line tri_set;;
 
 
-let border tri_set =
-    let one_time_seen_triangle = ref [] in
-    let supp_triangle
-    let length = List.length tri_set in
-    for tri_index = 0 to (length - 1) do
-        if
+let border line_set =
+    let ots_set_line = ref empty() in
+    let sup_set_line = ref empty() in
+    let result_set_line = ref line_set in
+    let find_aux cur_line =
+        if  not (find cur_line sup_set_line) then
+            if find cur_line ots_set_line then
+                sup_set_line := cur_line::(!sup_set_line);
+        if not(find cur_line ots_set_line) then
+            ots_set_line := cur_line::(!sup_set_line);
+    in iter find_aux line_set;
+    let sup_aux cur_line =
+        if find sup_set_line cur_line then
+            suppress result_set_line cur_line
+    in iter sup_aux result_set_line;
+    result_set_line;;
+
+
+
 
 
 let add_point tri_set new_point =
