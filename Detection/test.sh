5b03dcaa42e90d88159f6c5cdc920bb6dc15fc60
Desauw <lauric.desauw@ens-rennes.fr>
2018-10-17 11:42:43 +0200

    Sauvgarde branch

--- Detection/test.sh
+++ Detection/test.sh
@@ -2,7 +2,7 @@
 
 echo Detection
 echo detec.ml ../common/Interfaces/detec.mli test.ml:
-tm ./*.cm*
+rm ./*.cm*
 ocamlc -c -o detec.cmi ../common/Interfaces/detec.mli
 ocamlc -c -o detec.cmo detec.ml
 ocamlc -a -o detec.cma detec.cmo
