f7b9af2b81124d4598a1cf74c080a7f4196d6316
PaulBastide <paul.bastide@ens-rennes.fr>
2018-10-17 14:40:25 +0200

    first commit

---
+++ Change/changement.ml
@@ -0,0 +1,13 @@
+let rec find l elm = match l with
+|  
+
+
+let border tri_set =
+    let one_time_seen_triangle = ref [] in
+    let supp_triangle
+    let length = List.length tri_set in
+    for tri_index = 0 to (length - 1) do
+        if
+
+
+let add_point tri_set new_point =
