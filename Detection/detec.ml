05b08aca663a0277bbea2f5f21d660a9b06b844d
Desauw <lauric.desauw@ens-rennes.fr>
2018-10-17 11:38:02 +0200

    test.sh

--- Detection/detec.ml
+++ Detection/detec.ml
@@ -1,18 +1,15 @@
-type point = {x: float; y: float};;
-type triangle = { p1 : point; p2 : point ; p3 : point};;
-type point_set = point list;;
-type triangle_set = triangle list;;
+#use "../common/main.ml";;
 
 let is_counterclockwise (p1:point) (p2:point) (p3:point) =
   let det = (p2.x-.p1.x)*.(p3.y-.p1.y) -. (p2.y -. p1.y)*.(p3.x-.p1.x) in
     det >= 0.
 ;;
 
-let in_circle (t:triangle) (p:point) =
+let in_circle (t:triangle) (curr_point:point) =
   let det1 = (t.p1.x)*.(t.p2.y) -. (t.p2.x)*.(t.p1.y) in
   let det2 = ((t.p1.x)*.(t.p1.x) +. (t.p1.y)*.(t.p1.y)) -. ((t.p2.x)*.(t.p2.x) +. (t.p2.y)*.(t.p2.y)) in
-  let det3 = (t.p3.x)*.(p.y) -. (p.x)*.(t.p3.y) in
-  let det4 = ((t.p3.x)*.(t.p3.x) +. (t.p3.y)*.(t.p3.y)) -. ((p.x)*.(p.x) +. (p.y)*.(p.y)) in
+  let det3 = (t.p3.x)*.(curr_point.y) -. (curr_point.x)*.(t.p3.y) in
+  let det4 = ((t.p3.x)*.(t.p3.x) +. (t.p3.y)*.(t.p3.y)) -. ((curr_point.x)*.(curr_point.x) +. (curr_point.y)*.(curr_point.y)) in
     if is_counterclockwise (t.p1) (t.p2) (t.p3)
     then (det1*.det4 -. det2*.det3) > 0.
     else (det1*.det4 -. det2*.det3) < 0. ;;
--- Detection/test.sh
+++ Detection/test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo Detection
-echo detec.ml ../common/Interfaces/detec.mli:
+echo detec.ml ../common/Interfaces/detec.mli test.ml:
+tm ./*.cm*
 ocamlc -c -o detec.cmi ../common/Interfaces/detec.mli
 ocamlc -c -o detec.cmo detec.ml
 ocamlc -a -o detec.cma detec.cmo
+ocaml test.ml
