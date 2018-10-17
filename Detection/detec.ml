c026c8f2a425bbfa6940721ddea04cc1e905e2e6
Desauw <lauric.desauw@ens-rennes.fr>
2018-10-17 10:39:55 +0200

    detex v1

--- Detection/detec.ml
+++ Detection/detec.ml
@@ -6,4 +6,11 @@ let is_counterclockwise (A:point) (B:point) (C:point) =
     det >= 0
 ;;
 
-let in_circle (t:triangle) (p:point) = ;;
+let in_circle (t:triangle) (p:point) =
+  let det1 = (t.A.x)*(t.B.y) - (t.B.x)*(t.A.y) in
+  let det2 = ((t.A.x)*(t.A.x) + (t.A.y)*(t.A.y)) - ((t.B.x)*(t.B.x) + (t.B.y)*(t.B.y)) in
+  let det3 = (t.C.x)*(t.D.y) - (t.D.x)*(t.C.y) in
+  let det4 = ((t.C.x)*(t.C.x) + (t.C.y)*(t.C.y)) - ((t.D.x)*(t.D.x) + (t.D.y)*(t.D.y)) in
+    if is_counterclockwise (t.A) (t.B) (t.C)
+    then (det1*det4 - det2*det3) > 0
+    else (det1*det4 - det2*det3) < 0 ;;
