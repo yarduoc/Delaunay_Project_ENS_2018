35c88bc12b4fc8823afea9ed02dcb2daf0684e31
Desauw <lauric.desauw@ens-rennes.fr>
2018-10-17 10:21:51 +0200

    update type

--- Detection/detec.ml
+++ Detection/detec.ml
@@ -1,9 +1,9 @@
 type point = {x: float; y: float};;
-type triangle = {p0 : point; p1 : point; p3 : point};;
+type triangle = {C : point; B : point; C : point};;
 
 let is_counterclockwise (A:point) (B:point) (C:point) =
   let det = (B.x-A.x)*(C.y-A.y) - (B.y - A.y)*(C.x-A.x) in
-    det >= 0 
+    det >= 0
 ;;
 
-let in_circle (t:triangle) (p:point) = true;;
+let in_circle (t:triangle) (p:point) = ;;
