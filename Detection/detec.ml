a1630143bea3f4ca39f4d04f02d7f93d3e3647e2
Desauw <lauric.desauw@ens-rennes.fr>
2018-10-17 18:35:58 +0200

    end of the branch

--- Detection/detec.ml
+++ Detection/detec.ml
@@ -7,35 +7,41 @@ let is_counterclockwise (p1:point) (p2:point) (p3:point) =  (*Is correct*)
     det >= 0.
 ;;
 
-let in_circle (t:triangle) (curr_point:point) =
-  let in_matrix =  [| [| (t.p1.x -. curr_point.x);
-                      (t.p1.y -. curr_point.y);
-                      ((t.p1.x -. curr_point.x)
-                         *.(t.p1.x -. curr_point.x)
-                         +.(t.p1.y -. curr_point.y)
-                         *.(t.p1.y -. curr_point.y)
+let in_circle (tri:triangle) (curr_point:point) =
+  let in_matrix =  [| [| (tri.p1.x -. curr_point.x);
+                      (tri.p1.y -. curr_point.y);
+                      ((tri.p1.x -. curr_point.x)
+                         *.(tri.p1.x -. curr_point.x)
+                         +.(tri.p1.y -. curr_point.y)
+                         *.(tri.p1.y -. curr_point.y)
                       )|];
-                  [|(t.p2.x -. curr_point.x);
-                    (t.p2.y -. curr_point.y);
-                    ((t.p1.x -. curr_point.x)
-                        *.(t.p2.x -. curr_point.x)
-                        +.(t.p2.y -. curr_point.y)
-                        *.(t.p2.y -. curr_point.y)
+                  [|(tri.p2.x -. curr_point.x);
+                    (tri.p2.y -. curr_point.y);
+                    ((tri.p1.x -. curr_point.x)
+                        *.(tri.p2.x -. curr_point.x)
+                        +.(tri.p2.y -. curr_point.y)
+                        *.(tri.p2.y -. curr_point.y)
                      )|];
-                  [|(t.p3.x -. curr_point.x);
-                    (t.p3.y -. curr_point.y);
-                    ((t.p1.x -. curr_point.x)
-                          *.(t.p3.x -. curr_point.x)
-                          +.(t.p3.y -. curr_point.y)
-                          *.(t.p3.y -. curr_point.y)
+                  [|(tri.p3.x -. curr_point.x);
+                    (tri.p3.y -. curr_point.y);
+                    ((tri.p1.x -. curr_point.x)
+                          *.(tri.p3.x -. curr_point.x)
+                          +.(tri.p3.y -. curr_point.y)
+                          *.(tri.p3.y -. curr_point.y)
                     )|] |]
                  in
 
   let det = det_3 in_matrix in print_float det;
-  if is_counterclockwise (t.p1) (t.p2) (t.p3)
+  if is_counterclockwise (tri.p1) (tri.p2) (tri.p3)
     then det > 0.
     else det < 0.
   ;;
 
-let to_modify_t (t_set:triangle_set) (curr_point:point) =
-  let 
+
+let to_modify_tri (tri_set:triangle_set) (curr_point:point) =
+  let tri_list = ref [] in
+  let apply p tri =
+        if in_circle tri p
+        then tri_list := tri::(!tri_list)
+    in iter (apply curr_point) tri_set;
+    (!tri_list);;
--- common/Interfaces/detec.mli
+++ common/Interfaces/detec.mli
@@ -1,8 +1,3 @@
-type point = { x : float; y : float; }
-type triangle = { p1 : point; p2 : point; p3 : point; }
-type point_set = point list
-type triangle_set = triangle list
-
-
 val is_counterclockwise : point -> point -> point -> bool
 val in_circle : triangle -> point -> bool
+to_modify_tri : triangle_set -> point -> triangle_set
