c0725647b37b927f582270b033c879ddc6a92887
yarduoc <alexmadrid@free.fr>
2018-10-19 17:27:33 +0200

    edit

--- Graphic/affichage_paul.ml
+++ Graphic/affichage_paul.ml
@@ -20,7 +20,7 @@ let wait_move t =
     with End -> ()
 ;;
 
-let run p_set1 =
+let run_morph p_set1 =
     let t = ref 0. in
     let p_set2 = delta_set p_set1 200. in
     open_graph " 1000x800-0+0";
