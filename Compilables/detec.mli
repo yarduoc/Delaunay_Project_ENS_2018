10de71bc2fc8527fdf04ea61ece0b043544b3186
Desauw <lauric.desauw@ens-rennes.fr>
2018-10-22 15:31:53 +0200

    save pull master

--- Compilables/detec.mli
+++ Compilables/detec.mli
@@ -2,4 +2,4 @@ val is_counterclockwise : Pointtriangle.point -> Pointtriangle.point -> Pointtri
 val in_circle : Pointtriangle.triangle -> Pointtriangle.point -> bool
 val to_modify_tri : Pointtriangle.triangle_set -> Pointtriangle.point -> Pointtriangle.triangle_set
 val nearest_point : Pointtriangle.point_set -> Pointtriangle.point -> Pointtriangle.point
-val nearest_morph_point : Pointtriangle.morph_set -> Pointtriangle.point -> Pointtriangle.point
+val nearest_morph_point : Pointtriangle.morph_point_set -> Pointtriangle.point -> Pointtriangle.point
--- Compilables/display.mli
+++ Compilables/display.mli
@@ -1,3 +1,4 @@
 val init_display : int -> int -> unit
 val draw_point : Pointtriangle.point_set -> unit
 val draw_triangle : Pointtriangle.triangle_set -> unit
+val run_morph : Pointtriangle.point_set -> unit
